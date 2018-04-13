8c22069ee410e2d757d4affd860167859e4a1157
brosenberg <brosenberg@mitre.org>
2018-04-13 18:33:08 -0400

    Add DlClassLoader

---
+++ detection/utils/include/DlClassLoader.h
@@ -0,0 +1,133 @@
+/******************************************************************************
+ * NOTICE                                                                     *
+ *                                                                            *
+ * This software (or technical data) was produced for the U.S. Government     *
+ * under contract, and is subject to the Rights in Data-General Clause        *
+ * 52.227-14, Alt. IV (DEC 2007).                                             *
+ *                                                                            *
+ * Copyright 2018 The MITRE Corporation. All Rights Reserved.                 *
+ ******************************************************************************/
+
+/******************************************************************************
+ * Copyright 2018 The MITRE Corporation                                       *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ * You may obtain a copy of the License at                                    *
+ *                                                                            *
+ *    http://www.apache.org/licenses/LICENSE-2.0                              *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ ******************************************************************************/
+
+#ifndef OPENMPF_CPP_COMPONENT_SDK_DLCLASSLOADER_H
+#define OPENMPF_CPP_COMPONENT_SDK_DLCLASSLOADER_H
+
+#include <string>
+#include <memory>
+#include <dlfcn.h>
+#include <MPFDetectionException.h>
+
+
+namespace MPF { namespace COMPONENT {
+
+    template<typename... Ts>
+    struct all_standard_layout;
+
+    template<typename Head, typename... Tail>
+    struct all_standard_layout<Head, Tail...> {
+        static const bool value = std::is_standard_layout<Head>::value && all_standard_layout<Tail...>::value;
+    };
+
+    template<>
+    struct all_standard_layout<> {
+        static const bool value = true;
+    };
+
+
+    template <typename DlClass>
+    class DlClassLoader {
+    public:
+
+        template <typename... CreatorArgs>
+        DlClassLoader(
+                const std::string &lib_path,
+                const std::string &creator_func_name,
+                const std::string &deleter_func_name,
+                CreatorArgs&&... creator_args)
+            : lib_handle_(dlopen(lib_path.c_str(), RTLD_NOW), dlclose)
+            , instance_(LoadClass(lib_handle_.get(), lib_path, creator_func_name, deleter_func_name,
+                                  std::forward<CreatorArgs>(creator_args)...))
+        {
+            static_assert(all_standard_layout<CreatorArgs...>::value,
+                      "One or more creator_args do not use standard layout so they cannot be used with a function loaded using dlopen.");
+        }
+
+        DlClass* operator->() const noexcept {
+            return instance_.operator->();
+        }
+
+
+    private:
+        using instance_ptr_t = std::unique_ptr<DlClass, void(*)(DlClass*)>;
+
+        std::unique_ptr<void, decltype(&dlclose)> lib_handle_;
+
+        instance_ptr_t instance_;
+
+
+        template <typename... CreatorArgs>
+        static instance_ptr_t LoadClass(
+                    void* lib_handle,
+                    const std::string &lib_path,
+                    const std::string &creator_func_name,
+                    const std::string &deleter_func_name,
+                    CreatorArgs... args) {
+
+            if (lib_handle == nullptr) {
+                throw std::runtime_error("Failed to open \"" + lib_path + "\" due to: " + dlerror());
+            }
+
+            auto create_instance_fn = LoadFunction<DlClass*(CreatorArgs...)>(lib_handle, creator_func_name);
+            auto delete_instance_fn = LoadFunction<void(DlClass*)>(lib_handle, deleter_func_name);
+
+            instance_ptr_t instance(nullptr, delete_instance_fn);
+            try {
+                instance = { create_instance_fn(std::forward<CreatorArgs>(args)...), delete_instance_fn };
+            }
+            catch (const MPFDetectionException &ex) {
+                throw ex;
+            }
+            catch (const std::exception &ex) {
+                throw std::runtime_error(
+                        "Failed to load class from \"" + lib_path + "\" because the creator function, \""
+                        + creator_func_name + "\", threw an exception: " + ex.what());
+            }
+
+            if (instance == nullptr) {
+                throw std::runtime_error(
+                        "Failed to load class from \"" + lib_path + "\" because the creator function, \""
+                        + creator_func_name + "\", returned null.");
+            }
+            return instance;
+        }
+
+
+        template <typename TFunc>
+        static TFunc* LoadFunction(void* lib_handle, const std::string &symbol_name) {
+            auto result = reinterpret_cast<TFunc*>(dlsym(lib_handle, symbol_name.c_str()));
+            if (result == nullptr) {
+                throw std::runtime_error("dlsym failed for " + symbol_name + ": " + dlerror());
+            }
+            return result;
+        }
+    };
+}}
+
+
+
+#endif //OPENMPF_CPP_COMPONENT_SDK_DLCLASSLOADER_H
