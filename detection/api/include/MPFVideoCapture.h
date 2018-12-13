15a3eb5ac4a2cb64ab32036dc369f7a220771c95
brosenberg42 <brosenberg@mitre.org>
2018-12-13 08:25:06 -0500

    Add MPFVideoCapture constructor that takes a single string parameter. (#73)

--- detection/api/include/MPFVideoCapture.h
+++ detection/api/include/MPFVideoCapture.h
@@ -57,6 +57,8 @@ namespace MPF { namespace COMPONENT {
         explicit MPFVideoCapture(const MPFVideoJob &videoJob, bool enableFrameTransformers=true,
                                  bool enableFrameFiltering=true);
 
+        explicit MPFVideoCapture(const std::string &videoPath);
+
 
         bool Read(cv::Mat &frame);
 
